1e1f49893ebad88152f9cdec2dcd019f563d97a5
Caio V Rodrigues <caio.vr12@gmail.com>
2024-05-08 21:58:17 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className={`flex flex-col justify-center items-center`}>
+        <h1>Hello World!</h1>
+
+        <h1>APIExplore</h1>
+      </div>
+    </main>
+  );
+};
